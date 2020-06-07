33fef8cb1c8096c00ad781965bca0a4e27ed47a4
Malcolm Wright <wrightmbw01@gmail.com>
2020-06-07 12:22:17 -0400

    test commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT | 8080;
+const port = process.env.PORT | 8080
 const app = express();
 
 // set up server express 
@@ -9,4 +9,4 @@ app.get(/.*/, function(req, res) {
 })
 
 app.listen(port);
-console.log("Server is started!");
+console.log("Server is started! Listening on port " + port);
